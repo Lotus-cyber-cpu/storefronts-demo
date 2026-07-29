f69fe98b73a614f17d0a627190e853ded830061e
Lotus-cyber-cpu <bobikforza@gmail.com>
2026-07-29 20:06:19 +0300

    Add roost, rostok, vela; replace placeholder tiles with real screenshots

---
+++ site/rostok/_next/static/dIiiflUWl13KOusOkpyaN/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/storefronts-demo/rostok//_next/:path+",
+        "destination": "/storefronts-demo/rostok/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
